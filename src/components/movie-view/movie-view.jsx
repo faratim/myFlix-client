ee04d828a037b7fd0a4744035a348b4581264157
faratim <104940465+faratim@users.noreply.github.com>
2022-09-05 15:01:43 -0400

    troubleshooting

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 
 export class MovieView extends React.Component {
 
+  componentDidMount() {
+    document.addEventListener('keypress', event => {
+      console.log(event.key);
+    });
+  }
+
   render() {
     const { movie, onBackClick } = this.props;
 
