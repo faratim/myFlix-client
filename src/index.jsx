2142b8120b657b5aa0cc5e626932cef0cdc6e739
faratim <104940465+faratim@users.noreply.github.com>
2022-09-27 15:36:43 -0400

    refactored for redux and cleaned comments

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,30 @@
+// REACT | REACT-DOM
 import React from "react";
 import ReactDOM from "react-dom";
-import Container from "react-bootstrap/Container";
-import { legacy_createStore as createStore } from "redux";
-import { Provider } from "react-redux";
-import moviesApp from "./reducers/reducers";
-import { devToolsEnhancer } from "redux-devtools-extension";
-import MainView from "./components/main-view/main-view";
+import { createRoot } from 'react-dom/client'
 
-import "./index.scss";
+// REACT-BOOTSTRAP
+import Container from 'react-bootstrap/Container';
+
+// REDUX
+import { legacy_createStore as createStore } from "redux";
+import { Provider } from 'react-redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
+
+// LOCAL IMPORTS
+import moviesApp from './reducers/reducers';
+import MainView from './components/main-view/main-view';
+import './index.scss';
+
+// VARIABLES
 const store = createStore(moviesApp, devToolsEnhancer());
-// Main component (will eventually use all the others)
+
+// MAIN COMPONENT
 class MyFlixApplication extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <Container>
+        <Container fluid>
           <MainView />
         </Container>
       </Provider>
@@ -22,8 +32,10 @@ class MyFlixApplication extends React.Component {
   }
 }
 
-// Finds the root of your app
-const container = document.getElementsByClassName("app-container")[0];
+// FIND ROOT
+const container = document.getElementsByClassName('app-container')[0];
 
-// Tells React to render your app in the root DOM element
-ReactDOM.render(React.createElement(MyFlixApplication), container);
+// REACT RENDER
+createRoot(container)
+  .render(React.createElement(MyFlixApplication));
+// ReactDOM.render(React.createElement(MyFlixApplication), container);
